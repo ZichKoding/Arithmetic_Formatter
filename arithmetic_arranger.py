4a762381b0ddda126a86f9b7ed7ff68185929954
Chris Zichko <75284822+ZichKoding@users.noreply.github.com>
2021-02-23 14:31:49 -0500

    Update arithmetic_arranger.py
    Some minor errors had to be fixed with the optional argument, solve, and with detecting if something other than digits were inserted.

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,6 +1,6 @@
-def arithmetic_arranger(problems, **kwargs):
-  if len(problems) >= 5:
-    quit('Error: Too many problems.')
+def arithmetic_arranger(problems, solve=None):
+  if len(problems) > 5:
+    return('Error: Too many problems.')
     
   tops = []
   bottoms = []
@@ -15,23 +15,24 @@ def arithmetic_arranger(problems, **kwargs):
         addprob = probs.split('+')
 
         # Quit program if its not only digits.
-        if int(addprob[0]) is False:
-          quit("Error: Numbers must only contain digits.")
-        if int(addprob[1]) is False:
-          quit("Error: Numbers must only contain digits.")
+        for ap in addprob[0]:
+          if ap.isdigit() is False:
+            return "Error: Numbers must only contain digits."
+        for ap in addprob[1]:
+          if ap.isdigit() is False:
+            return "Error: Numbers must only contain digits."
         # Quit program if too many digits.
         if len(addprob[0]) >= 5:
-          quit("Error: Numbers cannot be more than four digits.")
+          return "Error: Numbers cannot be more than four digits."
         if len(addprob[1]) >= 5:
-          quit("Error: Numbers cannot be more than four digits.")
+          return "Error: Numbers cannot be more than four digits."
         
-
         if len(addprob[0]) > len(addprob[1]):
           space = (2 + len(addprob[0]))
           tops.append((((2 * ' ') + addprob[0])))
           bottoms.append(('+' + (space - len(addprob[1]) - 1) * ' ' + addprob[1]))
           lines.append('-' * space)
-          add = str(int(addprob[0]) - int(addprob[1]))
+          add = str(int(addprob[0]) + int(addprob[1]))
           solved.append((((space - len(add))) * ' ' + add))
 
         elif len(addprob[1]) > len(addprob[0]):
@@ -39,7 +40,7 @@ def arithmetic_arranger(problems, **kwargs):
           tops.append(((((space - len(addprob[0])) * ' ') + addprob[0])))
           bottoms.append(('+' + ' ' + addprob[1]))
           lines.append('-' * space)
-          add = str(int(addprob[0]) - int(addprob[1]))
+          add = str(int(addprob[0]) + int(addprob[1]))
           solved.append((((space - len(add))) * ' ' + add))
 
         elif len(addprob[1]) == len(addprob[0]):
@@ -47,26 +48,25 @@ def arithmetic_arranger(problems, **kwargs):
           tops.append(((((2 * ' ') + addprob[0]))))
           bottoms.append(('+' + ' ' + addprob[1]))
           lines.append('-' * space)
-          add = str(int(addprob[0]) - int(addprob[1]))
+          add = str(int(addprob[0]) + int(addprob[1]))
           solved.append((((space - len(add))) * ' ' + add))
-
 
       # Subtraction Problems
       if prob == '-':
         subprob = probs.split('-')
-
         # Quit program if its not only digits
-        if int(subprob[0]) is False:
-          quit("Error: Numbers must only contain digits.")
-        if int(subprob[1]) is False:
-          quit("Error: Numbers must only contain digits.")
+        for sp in subprob[0]:
+          if sp.isdigit() is False:
+            return "Error: Numbers must only contain digits."
+        for sp in subprob[1]:
+          if sp.isdigit() is False:
+            return "Error: Numbers must only contain digits."
         # Quit program if too many digits
         if len(subprob[0]) >= 5:
-          quit("Error: Numbers cannot be more than four digits.")
+          return "Error: Numbers cannot be more than four digits."
         if len(subprob[1]) >= 5:
-          quit("Error: Numbers cannot be more than four digits.")
+          return "Error: Numbers cannot be more than four digits."
         
-
         if len(subprob[0]) > len(subprob[1]):
           space = (2 + len(subprob[0]))
           tops.append((((2 * ' ') + subprob[0])))
@@ -91,23 +91,19 @@ def arithmetic_arranger(problems, **kwargs):
           sub = str(int(subprob[0]) - int(subprob[1]))
           solved.append((((space - len(sub))) * ' ' + sub))
 
-
       # Quit program if using incorrect opperator.
       if prob == '/':
-        quit("Error: Operator must be '+' or '-'.")
+        return "Error: Operator must be '+' or '-'."
       if prob == '*':
-        quit("Error: Operator must be '+' or '-'.")
+        return "Error: Operator must be '+' or '-'."
       if prob == '**':
-        quit("Error: Operator must be '+' or '-'.")
+        return "Error: Operator must be '+' or '-'."
 
-
-    if kwargs.get('solve', False):
+    if solve is not None:
         arranged_problems = str(tops)[1:-1].replace(",", " "*3).replace("'", "") + '\n' + str(bottoms)[1:-1].replace(",", " "*3).replace("'", "") + '\n' + str(lines)[1:-1].replace(",", " "*3).replace("'", "") + '\n' + str(solved)[1:-1].replace(",", " "*3).replace("'", "")
-        
         arranged_problems = ''.join(arranged_problems)
     else:
         arranged_problems = str(tops)[1:-1].replace(",", " "*3).replace("'", "") + '\n' + str(bottoms)[1:-1].replace(",", " "*3).replace("'", "") + '\n' + str(lines)[1:-1].replace(",", " "*3).replace("'", "")
-
         arranged_problems = ''.join(arranged_problems)
 
   return arranged_problems
